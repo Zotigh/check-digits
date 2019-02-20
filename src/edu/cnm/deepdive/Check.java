b6214327f008ea43f4ff8a349b5a7ce59f00796b
Lance Zotigh <lancezotighlz@gmail.com>
2019-02-19 21:36:22 -0700

    Initial commit

---
+++ src/edu/cnm/deepdive/Check.java
@@ -0,0 +1,20 @@
+package edu.cnm.deepdive;
+
+public class Check {
+
+  public static void main(String[] args) {
+
+  }
+
+  public static int didgetSum(int a) {
+    String b = Integer.toString(a);
+    int c = 0;
+    for (int i = 0; i < b.length(); i++) {
+      String d = "";
+      i = Integer.parseInt(d) + c;
+      //parseInt(String b)
+    }
+    return c;
+  }
+
+}
