c1173602a4c85235fd9e872abf0ee4958f1e9955
Lance Zotigh <lancezotighlz@gmail.com>
2019-02-21 07:58:21 -0700

    pushing

--- src/edu/cnm/deepdive/Check.java
+++ src/edu/cnm/deepdive/Check.java
@@ -2,19 +2,23 @@ package edu.cnm.deepdive;
 
 public class Check {
 
+  private static final String OUTPUT_FORMAT =
+      "Input = %d; remainder = %d; digit sum = %d; remainder = %d.%n";
+  private static final int MODULUS = 9;
+
   public static void main(String[] args) {
-
+    int input = Integer.parseInt(args[0]);
+    int sum = digitSum(input);
+    System.out.printf(OUTPUT_FORMAT, input, input % MODULUS, sum, sum % MODULUS);
   }
 
-  public static int didgetSum(int a) {
-    String b = Integer.toString(a);
-    int c = 0;
-    for (int i = 0; i < b.length(); i++) {
-      String d = "";
-      i = Integer.parseInt(d) + c;
-      //parseInt(String b)
+
+  public static int digitSum(int a) {
+    int sum = 0;
+    String b = "" + a;
+    for (char c : b.toCharArray()) {
+      sum += c - '0';
     }
-    return c;
+    return sum;
   }
-
 }
